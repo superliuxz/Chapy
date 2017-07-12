413d8ed179e325ba31d44d3d523edcb1f8ab9730
superliuxz <superliuxz@gmail.com>
2017-07-11 23:26:00 -0700

    Add more user class methods

--- user.py
+++ user.py
@@ -1,15 +1,16 @@
 import socket, sys, json
+from UserInputParser import UserInputParser
 
 class User:
-	def __init__(self, HOST = socket.gethostname(), IP = 5000):
-		self.host = HOST
-		self.ip = IP
+	def __init__(self, host = socket.gethostname(), ip = 5000):
+		self.host = host
+		self.ip = ip
 		self.alias = None
 
-		s = socket.socket()
+		self.s = socket.socket()
 
 		try:
-			s.connect((self.host, self.ip))
+			self.s.connect((self.host, self.ip))
 		except:
 			print('Unable to connect {}@{}'.format(self.host, self.ip))
 			sys.exit()
@@ -19,7 +20,7 @@ class User:
 			tmp = sys.stdin.readline()
 			## send initial message to set alias
 			msg = json.dumps({"usr": "null", "verb": "/set_alias", "body": tmp.strip()})
-			s.send(bytes(msg, "utf-8"))
+			self.s.send(bytes(msg, "utf-8"))
 			## server returns the status of the request
 			success = s.recv(4096).decode("utf-8")
 
@@ -28,10 +29,22 @@ class User:
 			else:
 				print("The alias has been used!")
 
+			self.parser = UserInputParser(self.alias)
+
 	@staticmethod
 	def prompt():
 		sys.stdout.write('Me: ')
 		sys.stdout.flush()
+
+	def read_input(self):
+		input = sys.stdin.readline().strip()
+		msg = self.parser.parse(input)
+		return msg
+
+	def send_to_server(self, msg):
+		self.s.send(msg)
+
+
 
 if __name__ == "__main__":
 	u = User()
