f3f233fde68e2f8bad871ae9c89a9d8bb5a0fb7a
superliuxz <superliuxz@gmail.com>
2017-07-18 22:43:15 -0700

    fix TIME_WAIT state of the server socket

--- server.py
+++ server.py
@@ -24,6 +24,8 @@ class Server:
 
 
 		self.s = socket.socket()
+		## https://stackoverflow.com/questions/29217502/socket-error-address-already-in-use
+		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.s.bind((self.host, self.port))
 		self.s.listen(5)
 
